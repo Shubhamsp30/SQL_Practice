b7b7ea7512b9aba5a28860f90effb1fdd6a0d7ef
Shubhamsp30 <spshubhampatil30@gmail.com>
2026-01-09 21:19:49 +0530

    Basic Select & Filtering

--- BikeStore/queries/BASIC SELECT & FILTERING.sql
+++ BikeStore/queries/BASIC SELECT & FILTERING.sql
@@ -49,8 +49,11 @@ where phone='NULL'
 
 
 -- 7. Show products belonging to a specific category.
-select * from categories
-select * from products
+
+select * from products;
+select * from categories;
+
+select product_id,product_name,category_id from products
 
 SELECT
 p.product_id,
@@ -63,6 +66,52 @@ on c.category_id=p.category_id
 
 
 
-8. Find orders placed by a specific customer.
-9. List products with price between 500 and 1500.
-10. Display staff working in a specific store.
+-- 8. Find orders placed by a specific customer.
+
+-- 9. List products with price between 500 and 1500.
+select * from 
+products 
+where list_price BETWEEN 500 and 1500  
+
+-- 10. Display staff working in a specific store.
+select * from stores;
+select * from staffs;
+
+select 
+sf.staff_id,
+s.store_id,s.store_name
+from stores as s 
+join staffs as sf 
+on s.store_id=sf.store_id
+
+
+-- Advanced
+-- 11. Find customers whose last name starts with ‘S’.
+select * from customers
+
+select 
+*
+from customers 
+where last_name like 'S%'
+
+-- 12. List orders not yet shipped.
+select * from orders
+select * from customers
+select 
+*FROM
+orders
+where shipped_date = NULL
+
+
+-- 13. Find products not sold yet.
+select * from products
+select * from stocks
+
+select 
+DISTINCT 
+product_id
+from stocks
+
+
+14. Show customers from multiple cities using IN.
+15. Find orders placed in the last 30 days.
