fe54dfb7bb2055418d77c86ae301df6992f4060e
Shubhamsp30 <spshubhampatil30@gmail.com>
2026-01-13 01:14:52 +0530

    CTE

--- BikeStore/queries/SUBQUERIES.sql
+++ BikeStore/queries/SUBQUERIES.sql
@@ -18,15 +18,49 @@ having count(order_id)>(select avg(order_count)from (SELECT count(order_id) as o
 
 
 
-
-
-
 -- Orders with value higher than average order value.
-
+select * from order_items;
+select * from orders;
+select
+order_id,sum(quantity*list_price*(1-discount)) as order_value 
+from order_items 
+group by order_id 
+having sum(quantity*list_price*(1-discount))>
+(
+    SELECT AVG(order_value)
+    FROM (
+        SELECT 
+            SUM(quantity * list_price * (1 - discount)) AS order_value
+        FROM order_items
+        GROUP BY order_id
+    ) sub
+);
 
 -- Products belonging to most sold category.
+select * from order_items;
+select * from Products;
+select p.product_id,p.product_name,p.category_id from products as p where p.category_id=(
+select 
+  p.category_id
+from 
+     products as p
+join 
+     order_items as ot 
+on   p.product_id=ot.product_id 
+group by p.category_id
+order by sum(ot.quantity*ot.list_price*(1-ot.discount)) desc
+limit 1 
+)
+
 
 -- Stores with sales higher than best-performing store average.
+select * from order_items;
+select * from orders
+select o.store_id,sum(ot.quantity*ot.list_price*(1-discount)) as sales_by_store 
+from Orders as o join order_items as ot 
+on o.order_id=ot.order_id
+group by o.store_id  
+
 
 -- Advanced
 -- 6. Customers who spent more than city average.
