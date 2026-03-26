a41d8cb8562a0037d81f3c8cccf952dbe5ba8481
Shubhamsp30 <spshubhampatil30@gmail.com>
2026-03-26 22:35:05 +0530

    Hard_level

--- Dvdrental_Practice/JOINS/Hard.sql
+++ Dvdrental_Practice/JOINS/Hard.sql
@@ -39,28 +39,55 @@ select * from inventory;
 select * from rental;
 select * from film as f LEFT join inventory as i on f.film_id=i.film_id left join rental as r on i.inventory_id=r.inventory_id where r.rental_id is null ;
 
--- Show actors not in any film
+    -- Show actors not in any film
+select * from film_actor;
 select *from actor;
 select * from film;
-select * from film_actor;
+select * from actor as a left join film_actor as fa on a.actor_id=fa.actor_id where fa.actor_id is null 
+select * from actor as a where not exists(select * from film_actor as fa where fa.actor_id=a.actor_id) 
 
 -- Show customers with payment above overall average
-
+select * from payment;
+select *from customer;
+select customer_id,sum(amount)from payment group by customer_id having sum(amount)>(select avg(total)from(select sum(amount)as total from payment GROUP by customer_id))
 
 -- Show film category with highest number of films
-
+select * from film;
+select * from film_category;
+SELECT * from category;
+select fc.category_id,c.name,count(film_id) films from film_category as fc join category as c on fc.category_id=c.category_id group by fc.category_id,c.name order  by films desc limit 1
 
 -- Show customer with highest number of rentals
-
+select * from customer;
+select * from rental;
+select c.first_name,r.customer_id,count(r.rental_id) as highest_rental from rental as r join customer as c on c.customer_id=r.customer_id group by r. customer_id,c.first_name order by highest_rental desc limit 1
 
 -- Show films rented more than 10 times
+select * from inventory;
+select * from rental;
+SELECT f.film_id,
+       f.title,
+       COUNT(r.rental_id) AS rent_count
+FROM film f
+JOIN inventory i
+ON f.film_id = i.film_id
+JOIN rental r
+ON i.inventory_id = r.inventory_id
+GROUP BY f.film_id, f.title
+HAVING COUNT(r.rental_id) > 10
+ORDER BY rent_count DESC;
 
 
 -- Show staff with highest total payment handled
+select * from payment;
+select * from staff;
+select staff_id,sum(amount) as total_payment from payment group by staff_id ORDER by total_payment desc limit 1 
 
+select count(payment_id) as total_payment from payment GROUP by staff_id order by total_payment DESC limit 1
 
 -- Show customers with payments in last 30 days
-
+select * from payment
+select EXTRACT(day from payment_date) as date from payment
 
 -- Show films whose rental_rate > avg of their category
 
