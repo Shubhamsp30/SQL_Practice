c79ff139dd1ca12c59245a0da91e0b36cdf30f2c
Shubhamsp30 <spshubhampatil30@gmail.com>
2026-04-01 09:36:26 +0530

    Hard_Level

--- Dvdrental_Practice/JOINS/Hard.sql
+++ Dvdrental_Practice/JOINS/Hard.sql
@@ -3,7 +3,11 @@
 
 -- Show top 5 customers by total payment
 select * from payment
-select customer_id,sum(amount) as total_payment from payment group by customer_id order by total_payment desc limit 5
+select customer_id,sum(amount) as total_payment 
+from payment 
+group by customer_id 
+order by total_payment desc 
+limit 5
 
 -- Show second highest payment
 select DISTINCT amount from payment order by amount desc limit 1 offset 1
@@ -37,9 +41,11 @@ WHERE customer_id NOT IN (
 select * from film;
 select * from inventory;
 select * from rental;
-select * from film as f LEFT join inventory as i on f.film_id=i.film_id left join rental as r on i.inventory_id=r.inventory_id where r.rental_id is null ;
+select * from film as f LEFT join inventory as i on f.film_id=i.film_id 
+left join rental as r on i.inventory_id=r.inventory_id
+where r.rental_id is null ;
 
-    -- Show actors not in any film
+-- Show actors not in any film
 select * from film_actor;
 select *from actor;
 select * from film;
@@ -49,18 +55,28 @@ select * from actor as a where not exists(select * from film_actor as fa where f
 -- Show customers with payment above overall average
 select * from payment;
 select *from customer;
-select customer_id,sum(amount)from payment group by customer_id having sum(amount)>(select avg(total)from(select sum(amount)as total from payment GROUP by customer_id))
+select customer_id,sum(amount)from payment 
+group by customer_id 
+having sum(amount)>(select avg(total)from(select sum(amount)as total from payment GROUP by customer_id))
 
 -- Show film category with highest number of films
 select * from film;
 select * from film_category;
 SELECT * from category;
-select fc.category_id,c.name,count(film_id) films from film_category as fc join category as c on fc.category_id=c.category_id group by fc.category_id,c.name order  by films desc limit 1
+select fc.category_id,c.name,count(film_id) films from 
+film_category as fc join category as c on fc.category_id=c.category_id 
+group by fc.category_id,c.name 
+order by films desc 
+limit 1
 
 -- Show customer with highest number of rentals
 select * from customer;
 select * from rental;
-select c.first_name,r.customer_id,count(r.rental_id) as highest_rental from rental as r join customer as c on c.customer_id=r.customer_id group by r. customer_id,c.first_name order by highest_rental desc limit 1
+select c.first_name,r.customer_id,count(r.rental_id) as highest_rental from 
+rental as r join customer as c on c.customer_id=r.customer_id 
+group by r. customer_id,c.first_name 
+order by highest_rental desc 
+limit 1
 
 -- Show films rented more than 10 times
 select * from inventory;
@@ -82,7 +98,6 @@ ORDER BY rent_count DESC;
 select * from payment;
 select * from staff;
 select staff_id,sum(amount) as total_payment from payment group by staff_id ORDER by total_payment desc limit 1 
-
 select count(payment_id) as total_payment from payment GROUP by staff_id order by total_payment DESC limit 1
 
 -- Show customers with payments in last 30 days
@@ -101,19 +116,36 @@ where rental_rate>( select avg(f.rental_rate) as avg_rental from
 film as f join film_category as fc on f.film_id=fc.film_id )
 
 
--- Show films with same length as another film
-
-
 -- Show duplicate payment amounts
+SELECT amount,
+       COUNT(*) AS count
+FROM payment
+GROUP BY amount
+HAVING COUNT(*) > 1;
 
 
 -- Show customers who rented more than average rentals
+select * from rental;
+select * from customer;
+select customer_id,count(rental_id)as rental_count from rental 
+group by customer_id 
+having count(rental_id)>(select avg(cnt)from (select count(rental_id)as cnt from rental group by customer_id)1)
+
 
 
 -- Show film with highest rental count
 select * from film;
 select * from inventory;
 select * from rental;
+
+select f.film_id,count(r.rental_id) as Count_rental 
+from film as f join inventory as i on f.film_id=i.film_id 
+join rental as r on i.inventory_id=r.inventory_id 
+group by f.film_id
+order by Count_rental DESC
+limit 1 
+
+
 
 
 -- Show category with lowest films
@@ -131,7 +163,6 @@ LIMIT 1;
 -- Show customers who never rented
 select * from customer;
 select * from rental
-
 select * from customer as c left JOIN rental as r on c.customer_id=r.customer_id where r.rental_id is NULL 
 
 -- Show films with no inventory
