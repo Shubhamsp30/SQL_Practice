c1fcba0ecc5bcc4e7fda9fd6c35ab66b93600fcb
Shubhamsp30 <spshubhampatil30@gmail.com>
2026-04-07 21:32:57 +0530

    Intermediate

---
+++ Dvdrental_Practice/JOINS+Subquery +CTE+CASE_WHEN/INTERMEDIATE LEVEL.sql
@@ -0,0 +1,48 @@
+-- ✅ INTERMEDIATE LEVEL (15 Questions)
+
+
+-- Show customer name with total payment (JOIN + GROUP)
+select * from payment;
+select * from customer
+select c.first_name,sum(p.amount) as total_payment from payment as p join customer as c on p.customer_id=c.customer_id GROUP by c.first_name
+
+-- Show film title with total rentals
+select * from 
+
+-- Show category name with number of films
+
+
+-- Show staff with total payment handled
+
+
+-- Show customers who made more than 5 payments
+
+
+-- Show films rented more than 10 times
+
+
+-- Show customers with CASE → 'High Spender' if total > 150
+
+
+-- Show films with CASE → 'Long Movie' if length > 120
+
+
+-- Show films where rental_rate > avg rental_rate (subquery)
+
+
+-- Show payments greater than avg payment per customer
+
+
+-- Show customers whose total payment is greater than customer_id = 10
+
+
+-- Show films with max length per rating
+
+
+-- Show actors with number of films they acted in
+
+
+-- Show customers with number of rentals
+
+
+-- Show film title with category name using JOIN
---
+++ Dvdrental_Practice/JOINS+Subquery +CTE+CASE_WHEN/MEDIUM_LEVEL.sql
@@ -0,0 +1,77 @@
+-- ✅ MEDIUM LEVEL (20 Questions)
+
+-- 🔥 Logic building starts here
+
+
+-- Show top 5 customers by total payment (CTE)
+with customerss as (
+    select customer_id,sum(amount) toatl_payment from payment
+    group by customer_id
+    order by toatl_payment desc
+)
+select * from customerss limit 5
+
+-- Show second highest payment using subquery
+SELECT *
+FROM payment
+WHERE amount = (
+    SELECT MAX(amount)
+    FROM payment
+    WHERE amount < (
+        SELECT MAX(amount)
+        FROM payment
+    )
+)limit 1
+
+-- Show films longer than avg length per rating
+select * from film where length>(select avg(length)from film)
+
+-- Show customers who never made payment (LEFT JOIN)
+select * from customer;
+select * from payment
+select * from customer as c left JOIN payment as p on c.customer_id=p.customer_id where p.customer_id is NULL
+
+
+-- Show actors not assigned to any film
+
+
+-- Show customers whose payment > overall avg payment
+
+
+-- Show category with highest number of films
+
+
+-- Show customers with more rentals than average rentals
+
+
+-- Show films rented more than average rental count
+
+
+-- Show staff with highest total payment handled
+
+
+-- Show films where rental_rate > avg of their category
+
+
+-- Show customers categorized using CASE → VIP / Regular / Low
+
+
+-- Show films categorized using CASE → Long / Medium / Short
+
+
+-- Show customers whose total payment between 100 and 200
+
+
+-- Show film with highest rental count
+
+
+-- Show customers with max payment per store
+
+
+-- Show actors who worked in more than 5 films
+
+
+-- Show films with replacement_cost above avg
+
+
+-- Show customers whose rental count > 10
