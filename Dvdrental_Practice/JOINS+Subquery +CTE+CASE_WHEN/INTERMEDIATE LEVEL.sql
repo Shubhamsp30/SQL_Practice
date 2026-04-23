9ec1ed04563f083be7a717171d4ea47c54d5b556
Shubhamsp30 <spshubhampatil30@gmail.com>
2026-04-23 23:21:13 +0530

    Inter

--- Dvdrental_Practice/JOINS+Subquery +CTE+CASE_WHEN/INTERMEDIATE LEVEL.sql
+++ Dvdrental_Practice/JOINS+Subquery +CTE+CASE_WHEN/INTERMEDIATE LEVEL.sql
@@ -4,7 +4,9 @@
 -- Show customer name with total payment (JOIN + GROUP)
 select * from payment;
 select * from customer
-select c.first_name,sum(p.amount) as total_payment from payment as p join customer as c on p.customer_id=c.customer_id GROUP by c.first_name
+select c.first_name,sum(p.amount) as total_payment
+from payment as p join customer as c on p.customer_id=c.customer_id 
+GROUP by c.first_name
 
 -- Show film title with total rentals
 
